c2c7e991570cfa3a5db37d778354ff9c3d1ae4af
nordgaren <nordgarentv@gmail.com>
2026-03-10 00:12:45 -0700

    Update Enums to have 0 value no matter what. Add quotes to paths in HKXtoNAV.cs

--- JortPob/Model/HKXtoNAV.cs
+++ JortPob/Model/HKXtoNAV.cs
@@ -9,7 +9,7 @@ namespace JortPob.Model
         public static void HKXtoNAV(string hkxPath, string navPath, string settingsPath)
         {
             string gamePath = @$"{Const.ELDEN_PATH}\Game\eldenring.exe";
-            ProcessStartInfo startInfo = new(@$"{AppDomain.CurrentDomain.BaseDirectory}\ERNavmeshGenerator.exe", $"-g {gamePath} -i {hkxPath} -o {navPath} -s {settingsPath}" )
+            ProcessStartInfo startInfo = new(@$"{AppDomain.CurrentDomain.BaseDirectory}\ERNavmeshGenerator.exe", $"-g \"{gamePath}\" -i \"{hkxPath}\" -o \"{navPath}\" -s \"{settingsPath}\"" )
             {
                 WorkingDirectory = @$"{AppDomain.CurrentDomain.BaseDirectory}\",
                 UseShellExecute = false,
