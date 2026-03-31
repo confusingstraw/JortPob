62361662d5a664ff37f9c0f006fbf733ad356cd6
InfernoPlus <pmoriarty12@gmail.com>
2026-03-31 03:23:44 -0500

    mostly working now

--- JortPob/MusicSoundBank.cs
+++ JortPob/MusicSoundBank.cs
@@ -20,6 +20,7 @@ namespace JortPob
 
             if (Const.DEBUG_SKIP_SOUND) { return; }  // yep
 
+            /* Handle Morrowind Music */
             string[] exploreFiles = Directory.GetFiles(Path.Combine(Const.MORROWIND_PATH, "Data Files", "Music", "Explore"));
             string[] battleFiles = Directory.GetFiles(Path.Combine(Const.MORROWIND_PATH, "Data Files", "Music", "Battle"));
             List<string> files = new();
@@ -51,6 +52,9 @@ namespace JortPob
                 }
                 AddTrack(trackType, file);                    // convert music track and add to list
             }
+
+            /* Handle Skyrim Music */
+            // @TODO:
         }
 
         private void AddTrack(Track.Type type, string file)
@@ -113,73 +117,92 @@ namespace JortPob
 
             List<JsonNode> musicSegments = new(), musicTracks = new();
 
-            JsonNode templateSegment = templateJson[0];
-            JsonNode templateTrackExplore = templateJson[1];
-            JsonNode templateTrackCombat = templateJson[2];
-            JsonNode templatePlaylist = templateJson[3];
+            JsonNode templateContainer = templateJson[0];
+            JsonNode templateSegment = templateJson[1];
+            JsonNode templateTrack = templateJson[2];
+            JsonNode templateTree = templateJson[3];
+            JsonNode templatePlaylist = templateJson[4];
 
-            /* Find limgrave container we will be using */
-            JsonNode dayContainer = null, nightContainer = null;
-            foreach(JsonNode jsonNode in objects)
+            /* Get main switch */
+            int containerInsertAt = -1;
+            JsonNode musicSwitch = null;
+            for (int i=0;i<objects.Count;i++)
             {
+                JsonNode jsonNode = objects[i];
+
                 uint id = jsonNode["id"]?["Hash"]?.GetValue<uint>() ?? 0;
 
-                if (id == 936896166) { dayContainer = jsonNode; }
-                else if (id == 426838365) { nightContainer = jsonNode; }
-                
-                if(dayContainer != null && nightContainer != null) { break; }
+                if (id == 618709466) { musicSwitch = jsonNode; }
+                else if(id == 936896166) { containerInsertAt = i; }
             }
 
-            /* Delete 398617814 (MusicSegment for limgrave day) */
-            /*foreach(JsonNode jsonNode in objects)
-            {
-                uint id = jsonNode?["id"]?["Hash"].GetValue<uint>() ?? 0;
-                if (id == 398617814) { objects.Remove(jsonNode); break; }
-            }*/
+            /* Create and id containers */
+            JsonNode dayContainer = templateContainer.DeepClone();
+            JsonNode nightContainer = templateContainer.DeepClone();
+            JsonNode battleContainer = templateContainer.DeepClone();
+            dayContainer["id"]["Hash"] = globals.NextBnkId();
+            nightContainer["id"]["Hash"] = globals.NextBnkId();
+            battleContainer["id"]["Hash"] = globals.NextBnkId();
 
-            /* Remove limgrave track and playlist before we add custom nodes to it */
-            dayContainer["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Clear();
-            dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().RemoveAt(1);
+            /* Setup switch to use new containers */
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Clear(); // switch children
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Add(dayContainer["id"]["Hash"].GetValue<uint>());
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Add(nightContainer["id"]["Hash"].GetValue<uint>());
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Add(battleContainer["id"]["Hash"].GetValue<uint>());
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["count"] = musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Count;
+            musicSwitch["body"]["MusicSwitchContainer"]["arguments"].AsArray()[0]["group_id"] = 2670057292;  // decision tree args
+            musicSwitch["body"]["MusicSwitchContainer"]["arguments"].AsArray()[1]["group_id"] = 362379552;
+            JsonNode decisionTree = templateTree.DeepClone();
+            decisionTree["children"].AsArray()[0]["children"].AsArray()[0]["node_id"] = nightContainer["id"]["Hash"].GetValue<uint>();  // decision trees
+            decisionTree["children"].AsArray()[0]["children"].AsArray()[1]["node_id"] = dayContainer["id"]["Hash"].GetValue<uint>();
+            decisionTree["children"].AsArray()[1]["children"].AsArray()[0]["node_id"] = battleContainer["id"]["Hash"].GetValue<uint>();
+            musicSwitch["body"]["MusicSwitchContainer"]["tree"] = decisionTree;
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["transition_rules"].AsArray().Remove(2);
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["transition_rules"].AsArray().Remove(1);
+            musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["transition_rule_count"] = musicSwitch["body"]["MusicSwitchContainer"]["music_trans_node_params"]["transition_rules"].AsArray().Count;
+
+            /* Delete orphaned nodes (?) */
+            // @TODO: we should probably do this but its such a huge pain i'm not touching it for now
 
             /* Create nodes for our custom music */
             foreach (Track track in tracks)
             {
+                JsonNode container;
+                switch (track.type)
+                {
+                    case Track.Type.Night: container = nightContainer; break;
+                    case Track.Type.Battle: container = battleContainer; break;
+                    case Track.Type.Day: container = dayContainer; break;
+                    default: throw new System.Exception("I'm going to rip my dick off.");
+                }
+
                 JsonNode musicSegment = templateSegment.DeepClone();
-                JsonNode trackExplore = templateTrackExplore.DeepClone();
-                JsonNode trackCombat = templateTrackCombat.DeepClone();
-                JsonNode playlist = templatePlaylist.DeepClone();
+                JsonNode musicTrack = templateTrack.DeepClone();
+                JsonNode playlistEntry = templatePlaylist.DeepClone();
 
                 /* Create ids for nodes */
                 musicSegment["id"]["Hash"] = globals.NextBnkId();
-                trackExplore["id"]["Hash"] = globals.NextBnkId();
-                trackCombat["id"]["Hash"] = globals.NextBnkId();
-                playlist["playlist_item_id"] = globals.NextBnkId();
+                musicTrack["id"]["Hash"] = globals.NextBnkId();
+                playlistEntry["playlist_item_id"] = globals.NextBnkId();
 
                 /* Create playlist */
-                playlist["segment_id"] = musicSegment["id"]["Hash"].GetValue<uint>();
+                playlistEntry["segment_id"] = musicSegment["id"]["Hash"].GetValue<uint>();
 
                 /* Add segment and playlist to container */
-                dayContainer["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Add(musicSegment["id"]["Hash"].GetValue<uint>());
-                dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Add(playlist);
+                container["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"].AsArray().Add(musicSegment["id"]["Hash"].GetValue<uint>());
+                container["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Add(playlistEntry);
 
                 /* Fill out segment */
-                musicSegment["body"]["MusicSegment"]["music_node_params"]["children"]["items"][0] = trackExplore["id"]["Hash"].GetValue<uint>();
-                musicSegment["body"]["MusicSegment"]["music_node_params"]["children"]["items"][1] = trackCombat["id"]["Hash"].GetValue<uint>();
+                musicSegment["body"]["MusicSegment"]["music_node_params"]["children"]["items"][0] = musicTrack["id"]["Hash"].GetValue<uint>();
                 musicSegment["body"]["MusicSegment"]["markers"].AsArray()[1]["position"] = track.length; // length of track in millis
-                SillyJsonUtils.SortUInt(musicSegment["body"]["MusicSegment"]["music_node_params"]["children"]["items"]);
+                musicSegment["body"]["MusicSegment"]["music_node_params"]["node_base_params"]["direct_parent_id"] = container["id"]["Hash"].GetValue<uint>();
                 musicSegments.Add(musicSegment);
 
                 /* Fill out explore */
-                trackExplore["body"]["MusicTrack"]["node_base_params"]["direct_parent_id"] = musicSegment["id"]["Hash"].GetValue<uint>();
-                trackExplore["body"]["MusicTrack"]["sources"].AsArray()[0]["media_information"]["source_id"] = track.source;
-                trackExplore["body"]["MusicTrack"]["playlist"].AsArray()[0]["source_id"] = track.source;
-                musicTracks.Add(trackExplore);
-
-                /* Fill out combat */
-                trackCombat["body"]["MusicTrack"]["node_base_params"]["direct_parent_id"] = musicSegment["id"]["Hash"].GetValue<uint>();
-                trackCombat["body"]["MusicTrack"]["sources"].AsArray()[0]["media_information"]["source_id"] = track.source;
-                trackCombat["body"]["MusicTrack"]["playlist"].AsArray()[0]["source_id"] = track.source;
-                musicTracks.Add(trackCombat);
+                musicTrack["body"]["MusicTrack"]["node_base_params"]["direct_parent_id"] = musicSegment["id"]["Hash"].GetValue<uint>();
+                musicTrack["body"]["MusicTrack"]["sources"].AsArray()[0]["media_information"]["source_id"] = track.source;
+                musicTrack["body"]["MusicTrack"]["playlist"].AsArray()[0]["source_id"] = track.source;
+                musicTracks.Add(musicTrack);
 
                 /* Write wems */
                 string wemSrcPath = track.file;
@@ -189,34 +212,31 @@ namespace JortPob
                 File.Copy(wemSrcPath, wemTgtPath);
             }
 
-            /* Sort container */
+            /* Sort containers */
             SillyJsonUtils.SortUInt(dayContainer["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"]);
+            SillyJsonUtils.SortUInt(nightContainer["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"]);
+            SillyJsonUtils.SortUInt(battleContainer["body"]["MusicRandomSequenceContainer"]["music_trans_node_params"]["music_node_params"]["children"]["items"]);
 
             /* Modify playlist settings */
-            dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_item_count"] = dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Count;
-            JsonNode playlistSettings = dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray()[0];
-            playlistSettings["ers_type"] = 2;                // shuffle mode
-            playlistSettings["shuffle"] = 1;                // shuffle on
-            playlistSettings["loop_base"] = 0;             // 
-            playlistSettings["avoid_repeat_count"] = 1;   // 
-            playlistSettings["use_weight"] = 1;          // 
-            playlistSettings["child_count"] = dayContainer["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Count-1; // yep
+            void SetupPlaylist(JsonNode container)
+            {
+                container["body"]["MusicRandomSequenceContainer"]["playlist_item_count"] = container["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Count;
+                JsonNode playlistSettings = container["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray()[0];
+                playlistSettings["child_count"] = container["body"]["MusicRandomSequenceContainer"]["playlist_items"].AsArray().Count - 1; // yep
+            }
+            SetupPlaylist(dayContainer);
+            SetupPlaylist(nightContainer);
+            SetupPlaylist(battleContainer);
 
             /* Dump our new nodes into the bnk */
             foreach (JsonNode seg in musicSegments) { objects.Insert(0, seg); }
             foreach (JsonNode trk in musicTracks) { objects.Insert(0, trk); }
+            objects.Insert(containerInsertAt, dayContainer);
+            objects.Insert(containerInsertAt, nightContainer);
+            objects.Insert(containerInsertAt, battleContainer);
 
-            /* Need to move our state nodes (691596679) and (464936633) to the top of the list */
-            for(int i=0;i<objects.Count;i++)
-            {
-                uint id = objects[i]["id"]?["Hash"]?.GetValue<uint>() ?? 0;
-                if (id == 691596679 || id == 464936633)
-                {
-                    JsonNode node = objects[i];
-                    objects.RemoveAt(i);
-                    objects.Insert(0, node);
-                }
-            }
+            /* Add new nodes to indirect parent containers */
+            // @TODO: not required but we should fix this in the future for consistency
 
             /* Rebuild bnk */
             Directory.CreateDirectory(Path.GetDirectoryName(bnkJsonPath));
@@ -247,5 +267,22 @@ namespace JortPob
         {
             public enum Type { Day, Night, Battle }
         }
+
+        /*  :: Original structure of BgmPlaceInfo=0 aka 'Env_000_Green'
+        
+            Play_m0 (2303085137)
+	            Action (542216895)
+		            MusicSwitchContainer (1001573296)
+			            MusicSwitchContiner (618709466)
+				            MusicRandomSequenceContainer (426838365) [ Limgrave Night ]
+					            Music Segment (490586048)
+						            Music Track (1019675082) [ Peaceful ]
+						            Music Track (957430173) [ Combat ]
+				            MusicRandomSequenceContainer (936896166) [ Limgrave Day ]
+					            Music Segment (398617814)
+						            Music Track (138805394) [ Peaceful ]
+						            Music Track (633064261) [ Combat ]
+         
+        */
     }
 }
